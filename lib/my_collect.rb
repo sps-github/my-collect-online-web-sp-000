4d1e64cd50e6433565596819f0ce8c868abe897d
Sarah Peters <sarahpieters15@gmail.com>
2020-06-08 13:47:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,9 @@
-def my_collect(languages)
-
+def my_collect(lang)
+  counter = 0
+  collection = []
+  while counter < lang.length
+    collection << yield(lang[counter])
+    counter += 1
+  end
+  collection
 end
